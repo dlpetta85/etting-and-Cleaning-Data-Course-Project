6f02eed994f697776a720b22817979f7eaa173e9
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-02 14:23:55 -0300

    st2

--- settandclean.R
+++ settandclean.R
@@ -1,9 +1,20 @@
 library(dplyr)
 
+##collecting data
 features <- read.csv('C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\features.txt', col.names = c("n","functions"))
 
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+##naming the data base
+activity_labels <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\activity_labels.txt", col.names = c("code", "activity"))
 
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+subject_test <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\test\\testsubject_test.txt", col.names = "subject")
 
+x_test <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\X_test.txt", col.names = features$functions)
+
+y_test <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\y_test.txt", col.names = "code")
+
+subject_train <- read.table("C:\Users\r3\Desktop\UCI HAR Dataset\train\\trainsubject_train.txt", col.names = "subject")
+
+x_train <- read.table("C:\Users\r3\Desktop\UCI HAR Dataset\train\\X_train.txt", col.names = features$functions)
+
+y_train <- read.table("C:\Users\r3\Desktop\UCI HAR Dataset\train\\y_train.txt", col.names = "code")
 
