b5a45cecb48389758df090965f66e16e3e0b77dc
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-02 14:34:53 -0300

    st4

--- settandclean.R
+++ settandclean.R
@@ -1,9 +1,8 @@
 library(dplyr)
 
-##collecting data
-features <- read.csv('C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\features.txt', col.names = c("n","functions"))
-
 ##naming the data base
+features <- read.table('C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\features.txt', col.names = c("n","functions"))
+
 activity_labels <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\activity_labels.txt", col.names = c("code", "activity"))
 
 subject_test <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\test\\subject_test.txt", col.names = "subject")
@@ -12,10 +11,22 @@ x_test <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\test\\X_test.txt"
 
 y_test <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\test\\y_test.txt", col.names = "code")
 
-subject_train <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\train\\trainsubject_train.txt", col.names = "subject")
+subject_train <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\train\\subject_train.txt", col.names = "subject")
 
 x_train <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\train\\X_train.txt", col.names = features$functions)
 
 y_train <- read.table("C:\\Users\\r3\\Desktop\\UCI HAR Dataset\\train\\y_train.txt", col.names = "code")
+
+
+## 1 - Merges the training and the test sets to create one data set.
+
+
+## 2 - Extracts only the measurements on the mean and standard deviation for each measurement.
+
+## 3 - Uses descriptive activity names to name the activities in the data set
+
+## 4- Appropriately labels the data set with descriptive variable names.
+
+## 5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
